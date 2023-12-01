82ae7ca4cf7e90531374701437f3926c32a06dd2
bagusyanuar <bagus.yanuar613@gmail.com>
2023-12-01 22:28:52 +0700

    migration

---
+++ database/migrations/2023_12_01_152634_create_ts_se_equipment_images.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTsSeEquipmentImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ts_se_equipment_images', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('ts_special_equipment_id');
+            $table->string('image');
+            $table->timestamps();
+            $table->foreign('ts_special_equipment_id')->references('id')->on('ts_special_equipment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ts_se_equipment_images');
+    }
+}
