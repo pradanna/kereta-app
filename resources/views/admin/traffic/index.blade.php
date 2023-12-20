64b93a4cc994ec08c4abdc4bb191d586b1bfb338
pradanna <pradanamahendra@gmail.com>
2023-12-20 21:08:10 +0700

    icon baru

--- resources/views/admin/traffic/index.blade.php
+++ resources/views/admin/traffic/index.blade.php
@@ -14,28 +14,28 @@
         </nav>
     </div>
     <section class="pt-3">
-        <div class="row gx-3 justify-content-center">
+        <div class="row gx-3 justify-content-start">
             <div class="col-sm-12 col-md-3 col-lg-2 col-xl-2 mb-3">
                 <div class="card-menu" data-slug="stasiun-kereta-api">
-                    <span class="material-symbols-outlined">directions_railway</span>
-                    <span class="text-center">Stasiun Kereta Api</span>
+                    <span class="spanmenu"><img class="icon-menu"
+                            src="{{ asset('images/local/lalulintas/train-station.png') }}" /></span>
+                    <span class="text-center text-menu">Stasiun Kereta Api</span>
                 </div>
             </div>
         </div>
     </section>
-
 @endsection
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('/css/custom-style.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('/css/custom-style.css') }}" />
 @endsection
 
 @section('js')
     <script src="{{ asset('js/helper.js') }}"></script>
     <script>
         var path = '{{ route('traffic') }}';
-        $(document).ready(function () {
-            $('.card-menu').on('click', function () {
+        $(document).ready(function() {
+            $('.card-menu').on('click', function() {
                 let slug = this.dataset.slug;
                 window.location.href = path + '/' + slug;
             });
